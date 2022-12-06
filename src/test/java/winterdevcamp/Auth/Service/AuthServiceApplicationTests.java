729637f84cdd12dc1af0717b2424553c5102362c
KangMinjeong <a50070863@gmail.com>
2022-12-06 20:34:19 +0900

    Init 첫 커밋

---
+++ src/test/java/winterdevcamp/Auth/Service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package winterdevcamp.Auth.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
