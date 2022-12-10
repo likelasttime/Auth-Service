30d3cbc831d670eb430071b35cd56d96a451aff8
KangMinjeong <a50070863@gmail.com>
2022-12-10 23:53:56 +0900

    Feat: 추가 이메일 인증
    이메일 인증 페이지, 관련 컨트롤러 추가
    이메일 인증 서비스 추가 예정

--- src/main/java/winterdevcamp/Auth/Service/controller/HomeController.java
+++ src/main/java/winterdevcamp/Auth/Service/controller/HomeController.java
@@ -1,10 +1,21 @@
 package winterdevcamp.Auth.Service.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import winterdevcamp.Auth.Service.model.Member;
+import winterdevcamp.Auth.Service.service.AuthService;
+
+import java.security.Principal;
 
 @Controller
+@Slf4j
 public class HomeController {
+    @Autowired
+    private AuthService authService;
+
     @GetMapping("/user/signupform")
     public String signupForm(){
         return "/signup";
@@ -12,4 +23,17 @@ public class HomeController {
 
     @GetMapping("/login")
     public String login(){return "/login";}
+
+    @GetMapping("/authentication")
+    public String email_authentication(Principal principal, Model model){
+        String username = principal.getName();
+        try {
+            Member member = authService.findByUsername(username);
+            model.addAttribute("email", member.getEmail());
+            model.addAttribute("username", username);
+        }catch(Exception e){
+            log.info("사용자 정보가 없습니다.");
+        }
+        return "/authentication";
+    }
 }
