5e393559a6291509af51e6c3f964e3c7e149ba63
KangMinjeong <a50070863@gmail.com>
2023-01-01 00:00:57 +0900

    Feat(/gateway): 수정 CustomAuthFilter

--- gateway/src/main/java/com/example/gateway/filter/CustomAuthFilter.java
+++ gateway/src/main/java/com/example/gateway/filter/CustomAuthFilter.java
@@ -1,97 +1,70 @@
 package com.example.gateway.filter;
 
-import com.example.gateway.service.JwtUtil;
-import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Component
 public class CustomAuthFilter extends AbstractGatewayFilterFactory<CustomAuthFilter.Config> {
-    @Autowired
-    private JwtUtil jwtUtil;
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
 
-    public CustomAuthFilter() {
+    Environment env;
+    public CustomAuthFilter(Environment env) {
         super(Config.class);
+        this.env = env;
     }
+    public static class Config {
 
+    }
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            String[] cookies = exchange.getRequest().getHeaders().get("Cookie").get(0).split(";");
-            Map<String, String> map = new HashMap<>();
-            for(int i=0; i<cookies.length; i++){
-                String[] splitted = cookies[i].split("=");
-                map.put(splitted[0], splitted[1]);
-                if(splitted[0] == " accessToken")
-                    break;
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange, "no authorization header", HttpStatus.UNAUTHORIZED);
             }
 
-            String token = map.get("accessToken");
-            //String token = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7);
-            String userName = jwtUtil.getUsername(token);
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
 
-            addAuthorizationHeaders(exchange.getRequest(), userName);
+            isJwtValid(jwt);
 
             return chain.filter(exchange);
         };
     }
-
-    private void addAuthorizationHeaders(ServerHttpRequest request, String userName) {
-        request.mutate()
-                .header("X-Authorization-Id", userName)
-                .build();
-    }
-
-    @Bean
-    public ErrorWebExceptionHandler tokenValidation() {
-        return new JwtTokenExceptionHandler();
-    }
-
-    public class JwtTokenExceptionHandler implements ErrorWebExceptionHandler {
-        private String getErrorCode(int errorCode) {
-            log.info("JwtTokenExceptionHandler errorCode: " + errorCode);
-            return String.format("errorCode: %d", errorCode);
-        }
-
-        @Override
-        public Mono<Void> handle(
-                ServerWebExchange exchange, Throwable ex) {
-            int errorCode = 500;
-            if (ex.getClass() == NullPointerException.class) {
-                errorCode = 100;
-            } else if (ex.getClass() == ExpiredJwtException.class) {
-                errorCode = 200;
-            }
-
-            byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
-            DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
-            return exchange.getResponse().writeWith(Flux.just(buffer));
+    private void isJwtValid(String jwt) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(jwt);
+        } catch (SignatureException | MalformedJwtException |
+                UnsupportedJwtException | IllegalArgumentException | ExpiredJwtException jwtException) {
+            throw jwtException;
         }
     }
 
-    public static class Config {
-        private String baseMessage;
-        private boolean preLogger;
-        private boolean postLogger;
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
 
-        public Config(String baseMessage, boolean preLogger, boolean postLogger) {
-            this.baseMessage = baseMessage;
-            this.preLogger = preLogger;
-            this.postLogger = postLogger;
-        }
+        log.error(err);
+        return response.setComplete();
     }
 }
